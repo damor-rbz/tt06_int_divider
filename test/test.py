4d9bba47911f48b39232c2dea73a908e5e90dab9
Daniel Amor Martín <damor@rbz.es>
2024-02-24 10:59:11 +0100

    Tests configured, not doing much at the moment

--- test/test.py
+++ test/test.py
@@ -6,7 +6,7 @@ from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
 @cocotb.test()
-async def test_adder(dut):
+async def test_divider(dut):
   dut._log.info("Start")
   
   # Our example module doesn't use clock and reset, but we show how to use them here anyway.
@@ -29,4 +29,4 @@ async def test_adder(dut):
 
   await ClockCycles(dut.clk, 1)
 
-  assert dut.uo_out.value == 50
+  assert dut.uo_out.value == 0
