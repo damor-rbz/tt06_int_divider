3c6106e967f049ff24d5e61a4dd230b28b2c826b
djuara <djuara@rbz.es>
2024-03-06 11:26:16 +0100

    Añadido reset al diseño, y modificados los test para buscar flancos

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, RisingEdge, FallingEdge
 from cocotb.binary import BinaryRepresentation, BinaryValue
 
 ## Test coverage
@@ -17,42 +17,49 @@ from cocotb.binary import BinaryRepresentation, BinaryValue
 # 32 cases
 async def test_divider_cero(dut):
   # common init
-  dut._log.info("Start")
+  dut._log.info("Reset")
   clock = Clock(dut.clk, 10, units="us")
   cocotb.start_soon(clock.start())
   dut.ui_in.value = 0
+  dut.rst_n.value = 0
   await ClockCycles(dut.clk, 10)
-
+  dut.rst_n.value = 1
+  dut._log.info("Test")
   # Set the input values, wait one clock cycle, and check the output
   dut._log.info("Test 0 divider positive sign")
 
   for x in range(16):
+    await RisingEdge(dut.clk)
     dut.ui_in.value = x
-    await ClockCycles(dut.clk, 2) 
     print(dut.ui_in.value)
+    await FallingEdge(dut.clk)
     assert dut.uo_out.value == 0b11111111
 
   dut._log.info("Test 0 divider negative sign")
   for x in range(16):
+    await RisingEdge(dut.clk)
     dut.ui_in.value = 0b10000000 + x
-    await ClockCycles(dut.clk, 2)
     print(dut.ui_in.value)
+    await FallingEdge(dut.clk)
     assert dut.uo_out.value == 0b11111111
 
 @cocotb.test()
 async def test_dividend_cero(dut):
   # common init
-  dut._log.info("Start")
+  dut._log.info("Reset")
   clock = Clock(dut.clk, 10, units="us")
   cocotb.start_soon(clock.start())
   dut.ui_in.value = 0
+  dut.rst_n.value = 0
   await ClockCycles(dut.clk, 10)
+  dut.rst_n.value = 1
 
   dut._log.info("Test dividend cero positive sign")
   for x in range(16):
+    await RisingEdge(dut.clk)
     dut.ui_in.value = x*16
-    await ClockCycles(dut.clk, 2)
     print(x, dut.ui_in.value)
+    await FallingEdge(dut.clk)
     if ( x == 0):
       assert dut.uo_out.value == 0b11111111
     elif ( x == 8):
@@ -62,9 +69,10 @@ async def test_dividend_cero(dut):
 
   dut._log.info("Test dividend cero negative sign")
   for x in range(16):
+    await RisingEdge(dut.clk)
     dut.ui_in.value = 0b00001000 + x*16
-    await ClockCycles(dut.clk, 2)
     print(x, dut.ui_in.value)
+    await FallingEdge(dut.clk)
     if ( x == 0):
       assert dut.uo_out.value == 0b11111111
     elif ( x == 8):
@@ -75,18 +83,21 @@ async def test_dividend_cero(dut):
 @cocotb.test()
 async def test_divider_one(dut):
   # common init
-  dut._log.info("Start")
+  dut._log.info("Reset")
   clock = Clock(dut.clk, 10, units="us")
   cocotb.start_soon(clock.start())
   dut.ui_in.value = 0
+  dut.rst_n.value = 0
   await ClockCycles(dut.clk, 10)
+  dut.rst_n.value = 1
 
   # Set the input values, wait one clock cycle, and check the output
   dut._log.info("Test 0 divider positive sign")
 
   for x in range(16):
+    await RisingEdge(dut.clk)
     dut.ui_in.value = x + 16
-    await ClockCycles(dut.clk, 2)
+    await FallingEdge(dut.clk)
     print(dut.ui_in.value)
     print(dut.uo_out.value)
     #assert dut.uo_out.value == x 
